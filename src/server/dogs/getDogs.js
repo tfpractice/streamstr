5dddf906273a22fea8d0f5680e38089abe3a13ed
tfpractice <jackson.brebnor@gmail.com>
2018-04-03 13:03:43 -0400

    streams

--- src/server/dogs/getDogs.js
+++ src/server/dogs/getDogs.js
@@ -1,7 +1,70 @@
 import axios from "axios";
+import http from "http";
 
-export const DOG_URL = `https://dog.ceo/api/breeds/list/all`;
+// import { Observable } from "rx";
+import * as RX from "rx";
 
-export const getDogs = () => axios.get(DOG_URL);
+const { Observable, Node } = RX;
+
+const {
+  fromCallback,
+  fromNodeCallback,
+  fromEvent,
+  toEventEmitter,
+  fromStream,
+  fromReadableStream,
+  fromWritableStream,
+  fromTransformStream,
+  writeToStream,
+} = Node;
+
+export const DOG_URL = `http://dog.ceo/api/breeds/list/all`;
+
+export const getDogs = () =>
+  axios.get(DOG_URL).then(x => console.log(`x`, x.data));
+
+const hDogs = () => http.get(DOG_URL);
+
+const onResponse = req => Observable.fromEvent(req, `response`);
+
+const onData = res => Observable.fromEvent(res, `data`);
+
+const resHand = res => {
+  console.log(`res`);
+  return res;
+};
+
+const dataHand = d => {
+  console.log(`d`, d);
+};
+
+// const handData
+
+// const final = onResponse(hDogs()).subscribe(resHand);
+console.log(`typeof hDogs()`, typeof hDogs());
+
+// const reqOb$ = fromStream(hDogs());
+
+onResponse(hDogs()).subscribe(x => {
+  console.log(`x`, x);
+
+  // console.log(`fromStream(x)`, fromStream(x));
+  return fromStream(x).subscribe(y => console.log(`y`, y));
+});
+
+// const subscription = fromStream(hDogs(), `end`, `response`).subscribe(x => {
+//   console.log(x);
+// });
+//
+// console.log(`reqOb$`, reqOb$);
+
+// .map(resHand);
+
+// console.log(`final`, final);
+
+// .subscribe(dataHand) ;
+// const v = hDogs();
+
+// console.log(`v`, v);
 
 // export const getDogs
